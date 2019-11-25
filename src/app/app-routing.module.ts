5a4871b1246bfb6358190baee6db8175638a4a52
Benbarek Bouchra <hb_benbarek@esi.dz>
2019-11-25 23:13:14 +0100

    changes in choices display

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FormsComponent } from './forms/forms.component';
+import { ChoicesComponent } from './choices/choices.component';
+
+const routes:Routes=[
+  {path:'',component:SidebarComponent},
+  {path:'forms',component:FormsComponent},
+  {path:'choices',component:ChoicesComponent}
+]
+
+
+@NgModule({
+  imports: [
+   RouterModule.forRoot(routes)
+  ], 
+  exports:[]
+})
+export class AppRoutingModule { }
