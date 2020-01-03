a525e5085f0c174f6813c4aa75cc0576533fc714
mokeddemhalima <57575174+mokeddemhalima@users.noreply.github.com>
2020-01-03 10:10:41 +0100

    Update etudiants.service.ts

--- src/app/etudiants/etudiants.service.ts
+++ src/app/etudiants/etudiants.service.ts
@@ -10,26 +10,27 @@ import {catchError} from 'rxjs/operators';
 @Injectable()
 export class EtudiantService {
 
-  private url = 'http://localhost:3000';
+  private url1 = 'http://localhost:3000';
+  private url2 = 'http://localhost:4000';
   constructor(private http: HttpClient, private toastr: ToastrService, private router: Router) { }
  
  /**
   * C'est une methode qui envoie la requete http 
-  * à la route"/afficher"" (backend) qui récupére le contenu de la table étudiant de la bdd.
+  * à la route"/afficher"" (backend) qui récupère le contenu de la table étudiant de la bdd.
   * @returns un tableau de Etudiant.
   */
   getChoix() {
     return this
       .http
-      .get(`${this.url}/etud/afficher`);
+      .get(`${this.url1}/etud1/afficher`);
   }
 /**
  * Une methode qui envoie la requete http à la route "/ajouter" (backend) qui mis à jour les choix de l'étudiant.
- * @param data de type Formulaire
+ * @param data de type Etudiant
  * @param mail de type Email
  */
  ajouterChoix(data,mail) {
-    this.http.put(`${this.url}/etud/ajouter`, data)
+    this.http.put(`${this.url1}/etud1/ajouter`, data)
       .subscribe(
         (res) => {
           console.log(res);
@@ -45,10 +46,10 @@ export class EtudiantService {
   /**
    * Une méthode qui envoie une requete http à la
    * route envoyer email dans le backend.
-   * @param data de type Formulaire
+   * @param data de type Email
    */
   envoyerEmail(data){
-    this.http.post(`${this.url}/etud/email`, data)
+    this.http.post(`${this.url2}/etud2/email`, data)
       .subscribe(
         (res) => {
           console.log(res);
